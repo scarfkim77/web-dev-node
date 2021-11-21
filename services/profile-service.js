0c2e7d9a46513d546870d0f51315371e7cb74ee3
77 <ckq.0928@163.com>
2021-11-21 12:06:27 -0800

    a8 fa21

---
+++ services/profile-service.js
@@ -0,0 +1,19 @@
+let profile = require('../data/profile.json');
+
+module.exports = (app) => {
+    const getCurrentProfile = (req, res) => {
+        res.json(profile);
+    };
+    app.get('/api/profile', getCurrentProfile);
+
+    const editProfile = (req, res) => {
+        profile.firstName = req.body.firstName;
+        profile.lastName = req.body.lastName;
+        profile.bio = req.body.bio;
+        profile.location = req.body.location;
+        profile.website = req.body.website;
+        profile.dateOfBirth = req.body.dateOfBirth;
+        res.json(profile);
+    };
+    app.put('/api/edit', editProfile);
+};
